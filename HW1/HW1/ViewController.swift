40bbefb8e89605f6fb6b53bc6219eedb13b37178
minseopkim <22200078@gmail.com>
2023-09-25 00:28:09 +0900

    hw1 delete .git

---
+++ HW1/HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by 김민섭 on 2023/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
